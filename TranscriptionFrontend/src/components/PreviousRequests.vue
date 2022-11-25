fefdaf074c72ef6dac8a7a7843c27b04b7c82d35
1nsomnes <55066794+1nsomnes@users.noreply.github.com>
2022-11-24 23:18:21 -0800

    Redesign Previous Request List

--- TranscriptionFrontend/src/components/PreviousRequests.vue
+++ TranscriptionFrontend/src/components/PreviousRequests.vue
@@ -16,11 +16,18 @@ export default {
                 res.json().then(json => {
                     this.title = json['title']
                     this.info = json['data']
-                    this.status = json['progress']
+                    let statusInfo = json['progress']
+                    if(statusInfo === 'done') {
+                        this.status = '100%';
+                    } else {
+                        let percent = statusInfo['downloaded_bytes']/statusInfo['total_bytes'];
+                        percent = Math.round(percent * 1000)/10;
+                        this.status = percent + "%";
+                    }
                 })
             })
         }
-    }, 
+    },
     data() {
         return {
             title: "Unloaded title...",
@@ -28,30 +35,62 @@ export default {
             status: "Unloaded status..."
         }
     },
-    created: function() {
+    created: function () {
         this.updateInfo();
     }
 }
 </script>
 
 <style scoped>
-    #element {
-       background-color: rgb(70, 68, 68); 
-       padding: 10px;
-       border-radius: 8px;
-       margin: 10px 0px;
-    }
-    #element:hover {
-        cursor:pointer;
-    }
+#element {
+    background-color: rgb(70, 68, 68);
+    padding: 10px;
+    border-radius: 8px;
+    margin: 10px 0px;
+}
+#grid {
+    display: grid;
+    grid-template-columns: 50% 25% 10%;
+    column-gap: 10px;
+}
+
+#grid>.section1>h2 {
+    color:white;
+    text-decoration: underline;
+}
+
+#grid>.section1, #grid>.section3>h2  {
+    cursor: pointer;
+}
+
+#grid>.item>h2 {
+    font-size: 15px;
+    text-overflow: ellipsis;
+}
+#grid>.item>h3 {
+    font-size: 13px;
+    text-overflow: ellipsis;
+}
 </style>
 
-<template> 
+<template>
     <!-- TODO: Add more personal request title information -->
-    <div id="element" v-on:click="requestClicked">
-        {{ this.title}}
-        Id: {{ requestNumber }}
-        Info: {{ this.info }}
-        Status: {{ this.status }}
+    <div id="element">
+        <div id="grid">
+            <div class="item section1" v-on:click="requestClicked">
+                <h2> {{ this.title }} </h2>
+                <h3> Id: {{ requestNumber }} </h3>
+                <h3> Info: {{ this.info }} </h3>
+            </div>
+            <div class="item section2">
+                <h2>Status:</h2>
+                <h3>{{ this.status }}</h3>
+            </div>
+
+            <div class="item section3">
+                <h2 v-on:click="updateInfo">Refresh</h2>
+            </div>
+        </div>
+
     </div>
 </template>
